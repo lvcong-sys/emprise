d80e4345c1f3975c897a572410c09e21a3268fba
lvcong-sys <1774581114@qq.com>
2023-02-20 10:20:04 +0800

    初始化spring项目

---
+++ src/main/java/com/lvcong/emprise/EmpriseApplication.java
@@ -0,0 +1,13 @@
+package com.lvcong.emprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lvcong/emprise/EmpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvcong.emprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
